c4b44cfa9a7c6ec5a7edd5a5279bda336cf66e72
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-11-13 19:23:32 +0600

    Wed, Nov 13, 2019  7:23:31 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  title: "Stateful",
-  home:FavouriteCity(),
-));
+      title: "Stateful",
+      home: FavouriteCity(),
+    ));
 
-class FavouriteCity  extends StatefulWidget {
-
+class FavouriteCity extends StatefulWidget {
   @override
   State createState() {
     return _FavouriteCityState();
   }
 }
 
-class _FavouriteCityState extends State<FavouriteCity>{
-
-  var nameCity  = "";
+class _FavouriteCityState extends State<FavouriteCity> {
+  var nameCity = "";
 
   @override
   Widget build(BuildContext context) {
+    debugPrint("widget is created");
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Stateful example"),
-      ),
-      body:Container(
-        child: Column(
-          children:<Widget>[
+        appBar: AppBar(
+          title: Text("Stateful example"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(30.0),
+          child: Column(children: <Widget>[
             TextField(
-              onSubmitted: (String input){
-                nameCity = input;
+              onSubmitted: (String input) {
+                setState(() {
+                  nameCity = input;
+                  debugPrint("State set");
+                });
               },
             ),
-            Text(
-              "Your fav city is $nameCity",
-              style: TextStyle(fontSize: 20.0),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "Your fav city is $nameCity",
+                style: TextStyle(fontSize: 20.0),
+              ),
             )
-          ]
-        ),
-      )
-    );
+          ]),
+        ));
   }
-
 }
-
-
-
-
